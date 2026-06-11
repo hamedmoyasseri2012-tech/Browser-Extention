752db2529dc4c7e1a51113233db336aa76e4c1f4
Kooroshban <kooroshbanyadm@gmail.com>
2026-06-11 17:58:47 +0330

    v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Footer from "./Componet/Footer";
-import Section from "./Componet/Exsation_list/Section";
+import Section from "./Componet/Exstention_list/Section";
 import Header from "./Componet/Header";
 
 const App = () => {
