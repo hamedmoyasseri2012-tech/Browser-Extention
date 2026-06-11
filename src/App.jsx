bfb7a0ab4a68f39888a298ff05d9827ac34545d5
Kooroshban <kooroshbanyadm@gmail.com>
2026-06-11 17:41:33 +0330

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import Header from 
+import Header from "./Componet/Header"
+import Section from "./Componet/Section"
+import Footer from "./Componet/Footer"
 const App = () => {
   return (
     <>
       < Header/>
+      <Section/>
+      <Footer/>
     </>
   );
 };
