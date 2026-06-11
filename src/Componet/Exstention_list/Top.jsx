521bbe1db91870ce283f6cc79777d147b3608f65
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-11 19:05:04 +0330

    v1.2.2

--- src/Componet/Exstention_list/Top.jsx
+++ src/Componet/Exstention_list/Top.jsx
@@ -1,35 +1,29 @@
-
-
-
-const Top=()=>{
-
-    console.log("hello");
-    return (
-      <div className="flex justify-between">
-        <h1 className="text-header-color text-3xl">Extensions List (12)</h1>
-        <div className="flex gap-4">
-          <button
-            type="button"
-            className="py-2 px-4 bg-secondary-button-color cursor-pointer rounded-2xl text-header-color"
-          >
-            All
-          </button>
-          <button
-            type="button"
-            className="py-2 px-4 bg-secondary-button-color cursor-pointer rounded-2xl text-header-color"
-          >
-            Active{"2"}
-          </button>
-          <button
-            type="button"
-            className="py-2 px-4 bg-secondary-button-color cursor-pointer rounded-2xl text-header-color"
-          >
-            Inactive{"1"}
-          </button>
-        </div>
+const Top = () => {
+  console.log("hello");
+  return (
+    <div className="flex justify-between pt-8">
+      <h1 className="text-header-color text-3xl">Extensions List (12)</h1>
+      <div className="flex gap-4">
+        <button
+          type="button"
+          className="py-2 px-4 bg-secondary-button-color cursor-pointer rounded-2xl text-header-color"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          className="py-2 px-4 bg-secondary-button-color cursor-pointer rounded-2xl text-header-color"
+        >
+          Active{"2"}
+        </button>
+        <button
+          type="button"
+          className="py-2 px-4 bg-secondary-button-color cursor-pointer rounded-2xl text-header-color"
+        >
+          Inactive{"1"}
+        </button>
       </div>
-    ); 
-
-
-}
+    </div>
+  );
+};
 export default Top;
