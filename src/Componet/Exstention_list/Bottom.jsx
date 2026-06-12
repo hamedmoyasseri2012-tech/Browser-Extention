8f3ee159c526d7e2ba771aeb0fc8fbb0a8c914fc
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-12 04:20:17 +0330

    v1.2.9

--- src/Componet/Exstention_list/Bottom.jsx
+++ src/Componet/Exstention_list/Bottom.jsx
@@ -1,24 +1,28 @@
 const Bottom = ({ dC }) => {
-  console.log("data Center:", dC);
+  // console.log("data Center:", dC);
   const renderExtensions = (extensionsList) => {
-    const name = extensionsList.name;
-    const logo = extensionsList.logo;
-    const description = extensionsList.description;
-    extensionsList.map((items, index) => (
-      <section>
+    // const name = extensionsList.name;
+    // const logo = extensionsList.logo;
+    // const description = extensionsList.description;
+
+    // برای گرفتن item name logo , item.name و ...
+    // استفاده شد
+    //
+    return extensionsList.map((items, index) => (
+      <section key={index} className="w-full">
         <div className="flex flex-col relative justify-center items-center gap-4 pl-4 pr-4 border bg-[#2f354b] rounded-xl w-100 h-59">
           <div className="flex gap-4 ">
             <img
               className="w-15 h-15 justify-center items-center mt-7 "
-              src="{logo}"
+              src="{items.logo}"
               alt=""
             />
             <div className="gap-4 mb-8 ">
               <h1 className="w-69 h-[22] text-xl font-bold text-[#e4f1f6]">
-                {name}
+                {items.name}
               </h1>
               <p className="w-69 h-16.75 text-xl text-[#9db5cf] font-bold">
-                {description}
+                {items.description}
               </p>
             </div>
           </div>
@@ -38,7 +42,7 @@ const Bottom = ({ dC }) => {
     ));
   };
   return (
-    <div className="grid grid-cols-2 gap-4 mt-4 justify-center items-center px-22 ">
+    <div className="grid grid-cols-2  gap-4 mt-4 justify-center items-center px-22 ">
       {renderExtensions(dC)}
     </div>
   );
