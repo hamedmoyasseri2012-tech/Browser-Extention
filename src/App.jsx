728f1616297917dacb5a605207fc8f3c3e748fad
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-12 22:59:02 +0330

    v1.2.15

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Footer from "./Componet/Footer";
 import Section from "./Componet/Exstention_list/Section";
 import Header from "./Componet/Header";
-
+import MessageBox from "./Componet/MessageBox";
 import data from "../public/data.json";
 import { useState } from "react";
+
+
 const App = () => {
   const source = JSON.stringify(data);
   const [info, setInfo] = useState(data);
 
+
+  
   // console.log("information: ",info);
 
   // console.log("source: ",source);
@@ -15,8 +19,8 @@ const App = () => {
     <>
       <Header />
       <Section data={info} />
-
       <Footer />
+      <MessageBox />
     </>
   );
 };
